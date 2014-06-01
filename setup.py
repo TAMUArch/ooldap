668b86382938289ce049948c0719c25bf266be9c
John Phillips <johnphillips@arch.tamu.edu>
2014-06-01 16:39:07 -0500

    metadata

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='ooldap',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ooldap', 'ooldap.objects'],
     url='http://github.com/TAMUArch/ooldap',
     author='John',
     author_email='st_jphillips@tamu.edu',
+    description='Object Oriented LDAP',
 )
