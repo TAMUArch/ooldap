0735d13994272d72366f26a2ac682401289ca697
John Phillips <johnphillips@arch.tamu.edu>
2014-06-01 17:05:05 -0500

    packages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name='ooldap',
     version='0.0.2',
-    packages=['ooldap', 'ooldap.objects'],
+    packages=find_packages(),
     url='http://github.com/TAMUArch/ooldap',
     author='John',
     author_email='st_jphillips@tamu.edu',
