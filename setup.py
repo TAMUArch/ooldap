0678c9f508fc15925519a31e61692cad0b61c651
John Phillips <johnphillips@arch.tamu.edu>
2014-06-01 17:08:17 -0500

    version metadata for 0.0.3

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+
+version = '0.0.3'
+
 setup(
     name='ooldap',
-    version='0.0.2',
+    version=version,
     packages=find_packages(),
     url='http://github.com/TAMUArch/ooldap',
     author='John',
